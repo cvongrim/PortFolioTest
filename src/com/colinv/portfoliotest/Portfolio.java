19f9dda91565f64de49234a9442c4b82870bcd37
Colin <colinvong@gmail.com>
2013-08-19 13:18:02 -0400

    Load multiple fragments

--- src/com/colinv/portfoliotest/Portfolio.java
+++ src/com/colinv/portfoliotest/Portfolio.java
@@ -6,18 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ColinV
- * Date: 8/14/13
- * Time: 11:37 AM
- * To change this template use File | Settings | File Templates.
- */
-public class TestFragment extends Fragment {
+public class Portfolio extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.yellow_xiik_x, container, false);
+        return inflater.inflate(R.layout.portfolio, container, false);
     }
 }
--- src/com/colinv/portfoliotest/main.java
+++ src/com/colinv/portfoliotest/main.java
@@ -3,6 +3,7 @@ package com.colinv.portfoliotest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -20,6 +21,71 @@ public class main extends FragmentActivity {
     }
 
     public void showOurTeam(View view){
+        if(findViewById(R.id.contentRelativeLayout) != null){
+            // Create fragment and give it an argument specifying the article it should show
+            OurTeam ourTeamFragment = new OurTeam();
+
+            ourTeamFragment.setArguments(getIntent().getExtras());
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.contentRelativeLayout, ourTeamFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+
+            // Close the sub menu
+            hideMenu();
+        }
+
+    }
+
+    public void showOurPortfolio(View view){
+        if(findViewById(R.id.contentRelativeLayout) != null){
+            // Create fragment and give it an argument specifying the article it should show
+            Portfolio ourTeamFragment = new Portfolio();
+
+            ourTeamFragment.setArguments(getIntent().getExtras());
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.contentRelativeLayout, ourTeamFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+
+
+            // Close the sub menu
+            hideMenu();
+        }
+
+
+    }
+
+    public void showHome(View view){
+        if(findViewById(R.id.contentRelativeLayout) != null){
+            // Create fragment and give it an argument specifying the article it should show
+            TestFragment testFragment = new TestFragment();
+
+            testFragment.setArguments(getIntent().getExtras());
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.contentRelativeLayout, testFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        }
+
 
 
     }
@@ -31,6 +97,14 @@ public class main extends FragmentActivity {
             subMenuLinearLayout .setVisibility(View.GONE);
         else
             subMenuLinearLayout .setVisibility(View.VISIBLE);
+    }
+
+    public void hideMenu(){
+        LinearLayout subMenuLinearLayout = (LinearLayout) findViewById(R.id.subMenuLinearLayout);
+
+        if(subMenuLinearLayout .getVisibility() == View.VISIBLE)
+            subMenuLinearLayout .setVisibility(View.GONE);
+
     }
 
 }
